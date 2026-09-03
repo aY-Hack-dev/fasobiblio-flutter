c38075d9d63f90681f98fb322201c7c165110d7c
Overdev <abimaelyameogo@gmail.com>
2026-09-03 22:50:03 +0000

    Feature: persist reading progress and bookmarks

--- lib/services/local_store.dart
+++ lib/services/local_store.dart
@@ -12,6 +12,7 @@ class LocalStore {
   static const notificationsKey = 'fasobiblio.flutter.notifications';
   static const notificationReadsKey = 'fasobiblio.flutter.notificationReads';
   static const welcomeSeenKey = 'fasobiblio.flutter.welcomeSeen';
+  static const readingStateKey = 'fasobiblio.flutter.readingState';
 
   Future<Set<String>> load(String key) async => (await SharedPreferences.getInstance()).getStringList(key)?.toSet() ?? <String>{};
   Future<void> save(String key, Set<String> values) async => (await SharedPreferences.getInstance()).setStringList(key, values.toList());
@@ -70,6 +71,15 @@ class LocalStore {
   }
 
   Future<void> saveJson(String key, Object value) async => (await SharedPreferences.getInstance()).setString(key, jsonEncode(value));
+
+  Future<Map<String, dynamic>> loadReadingState() async {
+    final value = await loadJson(readingStateKey);
+    if (value is! Map) return {};
+    return Map<String, dynamic>.from(value);
+  }
+
+  Future<void> saveReadingState(Map<String, dynamic> value) => saveJson(readingStateKey, value);
+
   Future<bool> loadWelcomeSeen() async => (await SharedPreferences.getInstance()).getBool(welcomeSeenKey) ?? false;
   Future<void> saveWelcomeSeen() async => (await SharedPreferences.getInstance()).setBool(welcomeSeenKey, true);
 }
